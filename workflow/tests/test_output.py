00315e7239ffc356310bab704214310b0a510c74
Jeremy Mathews <Jeremy.Mathews@utsouthwestern.edu>
2019-06-12 07:45:35 -0500

    fix encoding in test_output.py

--- workflow/tests/test_output.py
+++ workflow/tests/test_output.py
@@ -10,7 +10,7 @@ test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
 def test_output_VCF():
     assert os.path.exists(os.path.join(test_output_path, 'GM12878/GM12878.annot.vcf.gz'))
     annot_vcf = test_output_path + 'GM12878/GM12878.annot.vcf.gz'
-    annot_output = open(annot_vcf).readlines()
+    annot_output = open(annot_vcf, encoding="utf-8").readlines()
     assert 'FILTER=<ID=PASS,Description="All filters passed">' in annot_output[1]
 
 @pytest.mark.output
@@ -21,7 +21,7 @@ def test_output_Coverage_Histogram():
 @pytest.mark.output
 def test_output_CDF():
     assert os.path.exists(os.path.join(test_output_path, 'coverage_cdf.png'))
-    assert os.path.getsize(os.path.joint(test_output_path, 'coverage_cdf.png')) > 0
+    assert os.path.getsize(os.path.join(test_output_path, 'coverage_cdf.png')) > 0
 
 @pytest.mark.output
 def test_output_QC():
