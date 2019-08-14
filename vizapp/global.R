3202da5df5b3c857fb23e025e1a614219016b00b
Jeremy Mathews <Jeremy.Mathews@utsouthwestern.edu>
2019-08-14 16:36:22 -0500

    Fix global.R in Vizapp

--- vizapp/global.R
+++ vizapp/global.R
@@ -5,7 +5,7 @@ setwd("../")
 
 # Load Data
 annot_vcf <- read_table(Sys.glob("workflow/output/*/*.annot.vcf.gz"))
-annot_vcf = annot_vcf[-grep("^#", annot_vcf$`##fileformat=VCFv4.2`),] %>%
+annot_vcf <- annot_vcf[-grep("^#", annot_vcf$`##fileformat=VCFv4.2`),] %>%
   separate(`##fileformat=VCFv4.2`, into = c("CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT", "GM12878"), sep = "\t")
 
 #Initialize Table
@@ -20,18 +20,18 @@ Final$ID <- annot_vcf$ID
 Final$Reference_Allele <- annot_vcf$REF
 Final$Alternate_Allele <- annot_vcf$ALT
 for (i in 1:length(annot_vcf$INFO)) {
-  Final$Gene_Variant[i] = ((gsub(";.*", "", substring(annot_vcf$INFO[i], regexpr("ANN", annot_vcf$INFO[i]) + 4)) %>% strsplit(",") %>% unlist())[1] %>%
+  Final$Gene_Variant[i] <- ((gsub(";.*", "", substring(annot_vcf$INFO[i], regexpr("ANN", annot_vcf$INFO[i]) + 4)) %>% strsplit(",") %>% unlist())[1] %>%
                              strsplit("\\|") %>% unlist())[4]
-  Final$Exon_Number[i] = (((gsub(";.*", "", substring(annot_vcf$INFO[i], regexpr("ANN", annot_vcf$INFO[i]) + 4)) %>% strsplit(",") %>% unlist())[1] %>%
+  Final$Exon_Number[i] <- (((gsub(";.*", "", substring(annot_vcf$INFO[i], regexpr("ANN", annot_vcf$INFO[i]) + 4)) %>% strsplit(",") %>% unlist())[1] %>%
                             strsplit("\\|") %>% unlist())[9] %>% strsplit("/") %>% unlist())[1]
-  Final$Effects[i] = ((gsub(";.*", "", substring(annot_vcf$INFO[i], regexpr("ANN", annot_vcf$INFO[i]) + 4)) %>% strsplit(",") %>% unlist())[1] %>%
+  Final$Effects[i] <- ((gsub(";.*", "", substring(annot_vcf$INFO[i], regexpr("ANN", annot_vcf$INFO[i]) + 4)) %>% strsplit(",") %>% unlist())[1] %>%
                         strsplit("\\|") %>% unlist())[2]
-  Final$Alt_Percent[i] = ((strsplit(annot_vcf$GM12878[i], ":") %>% unlist())[4] %>% strtoi())/((strsplit(annot_vcf$GM12878[i], ":") %>% unlist())[2] %>% strtoi()) *100
-  Final$Alt_Percent = round(Final$Alt_Percent, digits = 2)
+  Final$Alt_Percent[i] <- ((strsplit(annot_vcf$GM12878[i], ":") %>% unlist())[4] %>% strtoi())/((strsplit(annot_vcf$GM12878[i], ":") %>% unlist())[2] %>% strtoi()) *100
   if (grepl("DP", annot_vcf$INFO[i])) {
-    Final$Total_Depth[i] = gsub(";.*", "", substring(annot_vcf$INFO[i], regexpr("DP", annot_vcf$INFO[i]) + 3))
+    Final$Total_Depth[i] <- gsub(";.*", "", substring(annot_vcf$INFO[i], regexpr("DP", annot_vcf$INFO[i]) + 3))
   }
   if (grepl("AF_POPMAX", annot_vcf$INFO[i])) {
-    Final$`Allele_Frequency(PopMax)`[i] = gsub(";.*", "", substring(annot_vcf$INFO[i], regexpr("AF_POPMAX", annot_vcf$INFO[i]) + 10))
+    Final$`Allele_Frequency(PopMax)`[i] <- gsub(";.*", "", substring(annot_vcf$INFO[i], regexpr("AF_POPMAX", annot_vcf$INFO[i]) + 10))
   }
 }
+Final$Alt_Percent <- round(Final$Alt_Percent, digits = 2)
